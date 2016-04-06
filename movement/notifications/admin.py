ceb396e86f69c2e4bb927c932e1a9f6cf468ab53
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-04-05 20:26:58 -0400

    list display for notifications

--- movement/notifications/admin.py
+++ movement/notifications/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import Notification
 
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('status', 'id', 'title', 'message', 'time_created', 'cohort')
+    list_display = ('id', 'title', 'message', 'time_created', 'cohort')
+    list_display_links = ('title',)
     model = Notification
 admin.site.register(Notification, NotificationAdmin)
