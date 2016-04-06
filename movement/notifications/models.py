e3934dfda517580a00132bd2454ee2be9396e980
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-04-05 20:42:17 -0400

    forgot import

--- movement/notifications/models.py
+++ movement/notifications/models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+import os
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
